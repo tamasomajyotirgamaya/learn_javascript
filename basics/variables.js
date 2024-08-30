9c63cd13836197ee7ca010b96e78a4a14995d357
Tushar Bhoge <tbhoge@guidepoint.com>
2024-08-31 00:59:36 +0530

    added variables file

---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 14422;
+let accountEmail = "tush.h@gmail.com";
+var accountPassword = "12312";
+
+accountCity = "Jaipur";
+
+
+accountEmail = "tushar1@gmail.com";
+accountPassword = "8892";
+accountCity = "bengaluru";
+let accountState;
+// accountId = 2; //not allowed
+
+// console.log(accountId);
+
+/*
+avoid using var
+*/
+console.table([accountEmail, accountCity, accountPassword, accountId, accountState])
