5a69687f998b1c66ce9d6bac6f4918df7e9074ee
Tushar Bhoge <tbhoge@guidepoint.com>
2024-09-05 17:13:50 +0530

    added conversion operation

---
+++ basics/conversionOperation.js
@@ -0,0 +1,34 @@
+/*
+let score = 33;
+let score1  = "33"
+let score2 = "33abc"
+console.log(typeof score);
+
+console.log(typeof score1);
+
+let valueInNumber = Number(score1);
+console.log(typeof valueInNumber); 
+
+let valueInNumber1 = Number(score2); //33abc
+console.log(valueInNumber1); // it will return NaN: Not a Number
+
+console.log(typeof NaN); //Number
+
+
+*/
+
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 -> true, 0-> false, "" -> false
+// "tushar" -> true
+
+
+let someNumber = 33;
+
+let stringSomeNumber = String(someNumber);
+console.log(typeof stringSomeNumber);
